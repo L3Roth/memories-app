db3c61d53cf26b99710eb8b9a10a8be743eaa3f3
Alexander Noack <alexander.noack95@outlook.de>
2022-06-18 16:14:09 +0200

    initial commit
    initial commit

---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getPosts, createPost } from '../controllers/posts.js'
+
+const router = express.Router();
+
+//http://localhost:5001/posts
+
+router.get('/', getPosts);
+router.get('/', createPost);
+
+export default router;
